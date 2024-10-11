773d37d76fad494edcec886cb18e1bb2e7b20b1c
ds4015 <166780943+ds4015@users.noreply.github.com>
2024-10-11 12:09:36 -0400

    Update Database.h
    wording

--- include/Database.h
+++ include/Database.h
@@ -46,16 +46,16 @@ public:
     const std::string query(std::string table, std::string selectColumns,
                             std::string filterColumn, std::string op, std::string value);
 
-    /* Overloaded query constructor:  Same as above, but with 2 filters
+    /* Overloaded query function:  Same as above, but with 2 filters
      *      e.g., SELECT selectColumns FROM table WHERE filterColumn1 op1 value1
      *              AND filterColumn2 op2 value2 */
     const std::string query(std::string table, std::string selectColumns,
                             std::string filterColumn1, std::string op1, std::string value1,
                             std::string filterColumn2, std::string op2, std::string value2);
 
-    /* Overloaded query constructor:  Same as above, but with 3 filters
+    /* Overloaded query function:  Same as above, but with 3 filters
      *      e.g., SELECT selectColumns FROM table WHERE filterColumn1 op1 value1
-                    AND filterColumn2 op2 value2 AND filterColumn3 op3 value3 */
+     *              AND filterColumn2 op2 value2 AND filterColumn3 op3 value3 */
     const std::string query(std::string table, std::string selectColumns,
                             std::string filterColumn1, std::string op1, std::string value1,
                             std::string filterColumn2, std::string op2, std::string value2,
