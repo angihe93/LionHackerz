3422dd4dc69e52c6d83f2526dc5cf524bf2cc0e1
Dallas Scott <ds4015@columbia.edu>
2024-10-12 12:39:57 -0400

    changed API key

--- src/main/Main.cpp
+++ src/main/Main.cpp
@@ -7,13 +7,13 @@ int main(void)
 {
 
     const std::string url = "https://alcpkkevodekihwyjzvl.supabase.co";
-    const std::string api = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFsY3Bra2V2b2Rla2lod3lqenZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg0MTQ2NjgsImV4cCI6MjA0Mzk5MDY2OH0.KbQQiYipuBqLL_0mmdEUvNQ3FIQmPKFpFo82K675Dms";
+    const std::string api = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFsY3Bra2V2b2Rla2lod3lqenZsIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyODQxNDY2OCwiZXhwIjoyMDQzOTkwNjY4fQ.qQaXij0b6rgniZpmsImn4AIC6Oh2OGUxFwJgpHbdeu4";
     Database *db = new Database(url, api);
 
     /* test query */
     int uid = 1;
-    
-    /* equivalent SQL query: "SELECT dim_id, weight_mod FROM Has_Augment WHERE id = 1" 
+
+    /* equivalent SQL query: "SELECT dim_id, weight_mod FROM Has_Augment WHERE id = 1"
      *  note that parameters must be stringified if not strings/string literals */
     std::string req = db->query("Has_Augment", "dim_id,weight_mod", "id", "eq", std::to_string(uid));
 
